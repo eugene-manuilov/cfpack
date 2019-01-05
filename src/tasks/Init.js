1e88ace17a55fd8c7804a319f7caaf33abc59f13
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-05 20:15:36 +0200

    Implemented init command

--- src/tasks/Init.js
+++ src/tasks/Init.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
 const Task = require('../Task');
 
 class Init extends Task {
 
 	run() {
-		
+		const filename = path.resolve(process.cwd(), 'cfpack.config.js');
+		const stream = fs.createWriteStream(filename, { encoding: 'utf8' });
+
+		stream.write(`module.exports = {
+	entry: "cloudformation", // folder with templates
+	output: "cloudformation.json", // resulting template file
+	verbose: true, // whether or not to display additional details
+	silent: false, // whether or not to prevent output from being displayed in stdout
+	stack: {
+		name: "my-stack", // stack name
+		region: "us-east-1", // stack region
+		params: {
+			// extra parameters that can be used by API, see details here:
+			// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudFormation.html#createStack-property
+		}
+	}
+};
+`);
+		stream.close();
 	}
 
 }
