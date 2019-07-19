75cfb1e17535bbdab0c9005695ecac744650317b
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-07-19 21:18:53 +0300

    Added bitbucket template

--- example/appsync-with-lambdas/cfpack.config.js
+++ example/appsync-with-lambdas/cfpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	entry: "cloudformation", // folder with templates
-	output: "cloudformation.json", // resulting template file
-	verbose: true, // whether or not to display additional details
-	silent: false, // whether or not to prevent output from being displayed in stdout
+	entry: "templates",
+	output: "cloudformation.json",
+	verbose: true,
+	silent: false,
 	stack: {
-		name: "my-stack", // stack name
-		region: "us-east-1", // stack region
+		name: "appsync-with-lambdas",
+		region: "us-east-1",
 		params: {
 			/**
 			 * Extra parameters that can be used by API
