6e479d7a6490d64c2cd9c28462fcb7aea56caaac
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-09-26 23:47:35 +0300

    Updated yaml schema to support condition functions

--- src/utils/intrinsic-functions-schema.js
+++ src/utils/intrinsic-functions-schema.js
@@ -1,52 +1,39 @@
 const yaml = require('js-yaml');
 
-module.exports = yaml.Schema.create([
-	new yaml.Type('!Base64', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::Base64': data }),
-	}),
-	new yaml.Type('!Cidr', {
-		kind: 'sequence',
-		construct: data => ({ 'Fn::Cidr': data }),
-	}),
-	new yaml.Type('!FindInMap', {
-		kind: 'sequence',
-		construct: data => ({ 'Fn::FindInMap': data }),
-	}),
-	new yaml.Type('!GetAtt', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::GetAtt': data }),
-	}),
-	new yaml.Type('!GetAZs', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::GetAZs': data }),
-	}),
-	new yaml.Type('!ImportValue', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::ImportValue': data }),
-	}),
-	new yaml.Type('!Join', {
-		kind: 'sequence',
-		construct: data => ({ 'Fn::Join': data }),
-	}),
-	new yaml.Type('!Select', {
-		kind: 'sequence',
-		construct: data => ({ 'Fn::Select': data }),
-	}),
-	new yaml.Type('!Split', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::Split': data }),
-	}),
-	new yaml.Type('!Sub', {
-		kind: 'scalar',
-		construct: data => ({ 'Fn::Sub': data }),
-	}),
-	new yaml.Type('!Transform', {
-		kind: 'mapping',
-		construct: data => ({ 'Fn::Transform': data }),
-	}),
-	new yaml.Type('!Ref', {
-		kind: 'scalar',
-		construct: data => ({ Ref: data }),
-	}),
-]);
+const types = [];
+const schema = {
+	scalar: [
+		'Base64',
+		'GetAtt',
+		'GetAZs',
+		'ImportValue',
+		'Split',
+		'Sub',
+		'Ref',
+	],
+	sequence: [
+		'Cidr',
+		'FindInMap',
+		'Join',
+		'Select',
+		'And',
+		'Equals',
+		'If',
+		'Not',
+		'Or',
+	],
+	mapping: [
+		'Transform',
+	],
+};
+
+Object.keys(schema).forEach((kind) => {
+	schema[kind].forEach((name) => {
+		const fn = name === 'Ref' ? name : 'Fn::' + name;
+		const type = new yaml.Type('!' + name, { kind, construct: data => ({ [fn]: data }) });
+
+		types.push(type);
+	});
+});
+
+module.exports = yaml.Schema.create(types);
