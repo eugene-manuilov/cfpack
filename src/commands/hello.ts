cf0e5149e68580b55a755bad18891615f663fd0c
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-08-23 00:49:34 +0300

    Updated eslint to work with typescript.

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,31 +1,33 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 export default class Hello extends Command {
-  static description = 'describe the command here'
 
-  static examples = [
-    `$ cfpack hello
-hello world from ./src/hello.ts!
-`,
-  ]
+	static description = 'describe the command here'
 
-  static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+	static examples = [
+		`$ cfpack hello
+	hello world from ./src/hello.ts!
+	`,
+	]
 
-  static args = [{name: 'file'}]
+	static flags = {
+		help: flags.help( { char: 'h' } ),
+		// flag with a value (-n, --name=VALUE)
+		name: flags.string( { char: 'n', description: 'name to print' } ),
+		// flag with no value (-f, --force)
+		force: flags.boolean( { char: 'f' } ),
+	}
 
-  async run() {
-    const {args, flags} = this.parse(Hello)
+	static args = [ { name: 'file' } ]
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
-  }
+	async run() {
+		const { args, flags } = this.parse( Hello );
+
+		const name = flags.name ?? 'world';
+		this.log( `hello ${ name } from ./src/commands/hello.ts` );
+		if ( args.file && flags.force ) {
+			this.log( `you input --force and --file: ${ args.file }` );
+		}
+	}
+
 }
